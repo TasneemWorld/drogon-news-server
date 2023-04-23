73124c2afb0ac7a983943069bc4fd140aef17e69
TasneemWorld <tasneemmatsust2017115@gmail.com>
2023-04-23 11:16:03 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 5000;
+
+const categories = require('./categories.json');
+const cros = require('cors')
+
+app.use(cros())
+
+
+app.get('/',(req, res)=>{
+    res.send('Drogon is running');
+});
+
+app.get('/categories',(req, res)=>{
+    res.send(categories);
+});
+
+app.listen(port, ()=>{
+    console.log(`Drogon API is running on port ${port}`)
+})
